f5b53f0c47cfc5cb33b5700b5b5891ef42a49d75
Sharath Mekottil <sharath.m@tavant.com>
2018-02-23 12:44:37 +0530

    Eureka client

--- src/main/java/com/ionwallet/expensetrackerusermanagement/ExpenseTrackerUserManagementApplication.java
+++ src/main/java/com/ionwallet/expensetrackerusermanagement/ExpenseTrackerUserManagementApplication.java
@@ -2,10 +2,12 @@ package com.ionwallet.expensetrackerusermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.ionwallet"})
+@EnableEurekaClient
 public class ExpenseTrackerUserManagementApplication {
 
 	public static void main(String[] args) {
